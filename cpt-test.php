1e78b66dc98408f74504cf6f565bbf4f4099a1da
thecorkboard <thecorkboard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-10 12:54:25 +0000

    changed version number
    git-svn-id: https://plugins.svn.wordpress.org/library-custom-post-types/trunk@449502 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cpt-test.php
+++ cpt-test.php
@@ -4,7 +4,7 @@ Plugin Name: Library Custom Post Types - TEST
 Plugin URI: http://thecorkboard.org/
 Description: A TEST content type.
 Author: Kyle Jones
-Version: 1.0
+Version: 1.1
 Author URI: http://thecorkboard.org
 */
 
