322e68aff94306f3dcc3a450ebd040c0c428efc6
thecorkboard <thecorkboard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-10 12:54:02 +0000

    changed version number
    git-svn-id: https://plugins.svn.wordpress.org/library-custom-post-types/trunk@449500 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cpt-databases.php
+++ cpt-databases.php
@@ -4,7 +4,7 @@ Plugin Name: Library Custom Post Types - Databases
 Plugin URI: http://thecorkboard.org/
 Description: A content type for listing databases.
 Author: Kyle Jones
-Version: 1.0
+Version: 1.1
 Author URI: http://thecorkboard.org
 */
 
