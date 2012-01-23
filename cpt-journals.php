c8c470b59f0d078a95516bbfe16eb9c61ecb27d4
thecorkboard <thecorkboard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-23 16:04:40 +0000

    Changed version number to 1.3 across all CPTs
    git-svn-id: https://plugins.svn.wordpress.org/library-custom-post-types/tags/1.3@494012 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cpt-journals.php
+++ cpt-journals.php
@@ -4,7 +4,7 @@ Plugin Name: Library Custom Post Types - Journals
 Plugin URI: http://thecorkboard.org/
 Description: A content type for listing journals.
 Author: Kyle Jones
-Version: 1.2
+Version: 1.3
 Author URI: http://thecorkboard.org
 */
 
